b1c3149804a3e741f093d6f82b6e1d85a6389aaf
nampdn <nampdn@protonmail.com>
2018-12-03 09:01:59 +0700

    build(rollup): add json plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 import pkg from './package.json';
 
 module.exports = {
@@ -11,6 +12,7 @@ module.exports = {
     format: 'cjs',
   },
   plugins: [
+    json(),
     typescript({
       tsconfig: "tsconfig.json"
     }),
